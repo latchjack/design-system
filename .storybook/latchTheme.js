ee8e35d40f41c51f5ec0ddd8ddee0bd115107d59
Latch Jackn <latch.jack@gmail.com>
2021-02-01 16:52:57 +0000

    adding theme

--- .storybook/latchTheme.js
+++ .storybook/latchTheme.js
@@ -2,25 +2,25 @@ import { create } from "@storybook/theming";
 
 export default create({
   base: "dark",
-  colorPrimary: "#6B1462",
-  colorSecondary: "#6B1462",
+  colorPrimary: "#1dc0e5",
+  colorSecondary: "#1dc0e5",
   // UI
-  appBg: "#1347a4", //6495ed cornflour blue
-  appContentBg: "#1e1e2f",
-  appBorderColor: "#27293d",
+  appBg: "#08498e",
+  appContentBg: "#174771",
+  appBorderColor: "#000000",
   appBorderRadius: 4,
   // Typography
   fontCode: "monospace",
   // Text colors
   textColor: "#ffffff",
-  textInverseColor: "white",
+  textInverseColor: "black",
   // Toolbar default and active colors
   barTextColor: "#9A9A9A",
-  barSelectedColor: "white",
-  barBg: "#6B1462",
+  barSelectedColor: "#1dc0e5",
+  barBg: "#000000",
   // Form colors
   inputBg: "transparent",
-  inputBorder: "#2b3553",
+  inputBorder: "#000000",
   inputTextColor: "#ffffff",
   inputBorderRadius: 4,
   brandTitle: "Latch Storybook",
