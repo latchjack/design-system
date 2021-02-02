f6a10776b9a92fc1fe1c082585f188cc10c2bd93
Latch Jackn <latch.jack@gmail.com>
2021-02-02 21:30:53 +0000

    added styled components

--- src/stories/ui/Checkbox/Checkbox.js
+++ src/stories/ui/Checkbox/Checkbox.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
+
+const StyledCheckbox = styled(Checkbox)`
+    &.MuiCheckbox-root,
+    &.Mui-checked {
+        background-color: blue;
+        color: green;
+    }
+`;
 
 const Chk = ({
     checked,
     disabled
 }) => (
-    <Checkbox
+    <StyledCheckbox
         checked={checked}
         disabled={disabled}
     />
